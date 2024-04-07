a0e812517afc94aa0d389534757629215b5b1e6d
leandromacedosilva <iamfulldeveloper@gmail.com>
2024-04-07 18:59:27 -0300

    sistema pede o nome do client

--- Client.java
+++ Client.java
@@ -1,8 +1,16 @@
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Client {
 	public static void main(String[] args) {
 		PrintStream pst = System.out;
-		pst.println("Hello world!");
+		Scanner input = new Scanner(System.in);
+
+		pst.println("INFORME OS DADOS DO CLIENTE");
+		
+		pst.print("NOME: ");
+		String nome = input.nextLine();
+
+		pst.println("NOME INFORMADO: " + nome);
 	}
 }
